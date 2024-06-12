e0cf055787e3af0b34e2f0aa8dbb9503367673c5
Jefferson Kleyton <p.kleyton71@gmail.com>
2024-06-12 13:06:03 -0300

    troca url

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://3.209.174.215/api/sundata'
+  baseURL: 'http://ec2-3-209-174-215.compute-1.amazonaws.com/api/sundata'
 })
