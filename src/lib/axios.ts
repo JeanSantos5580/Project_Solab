790ee2fb2613e0ac754c4d462c6a84f416b4063b
Jean Santos <dev.jean.ap@gmail.com>
2024-07-18 06:21:33 -0300

    chore: api base url config

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,9 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-<<<<<<< HEAD
   baseURL: 'https://api.webdev-js.com.br/api/sundata'
-=======
-  baseURL: 'http://ec2-3-209-174-215.compute-1.amazonaws.com/api/sundata'
->>>>>>> e0cf055787e3af0b34e2f0aa8dbb9503367673c5
 })
